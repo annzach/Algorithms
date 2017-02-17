74b9e6cbcc0866b667f5aa2f8a17b3be6f48fefe
annzach <annreenazach@gmail.com>
2017-02-17 13:07:35 -0800

    save changes

---
+++ arraysort.js
@@ -0,0 +1,24 @@
+/*We have an array of objects A and an array of indexes B. Reorder objects in array A with given indexes in array B. Do not change array A's length. 
+
+example:
+
+
+var A = [C, D, E, F, G];
+var B = [3, 0, 4, 1, 2];
+
+sort(A, B);
+// A is now [D, F, G, C, E]; */
+
+function sort(a, b) {
+  var c = new Array(a.length);
+  b.forEach((val, i) => {
+  c[val] = a[i]; 
+  console.log("value of i",i)
+  console.log("c after first foreach",c)
+  });
+
+  a.forEach((val, i) => { a[i] = c[i]; });
+  console.log(a);
+}
+
+sort(['c','d','e','f','g'],[3,0,4,1,2]);
