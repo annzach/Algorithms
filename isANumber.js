cdba50f12cc007595a8a98acddaa34bfe89b5214
annzach <annreenazach@gmail.com>
2017-02-18 15:26:43 -0800

    is a number

---
+++ isANumber.js
@@ -0,0 +1,15 @@
+//Write a function to determine if a string is a number without using any built-in function.
+
+function str(n){
+
+ if( x^0 == x)
+ return "number";
+ else
+ return "not a number"
+}
+// --or--
+//return (n>0) || (n < 0) || (n ==0)
+
+str("123.45");
+
+
